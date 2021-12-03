2bfe2aa155b5e4b2143c4478b43ba8785ac662ff
Joey <Joey@LAPTOP-A9BE0A28>
2021-12-03 11:04:41 -0600

    Problem 6 completed.

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -19,8 +19,8 @@ namespace DatabaseFirstLINQ
             //ProblemTwo();
             //ProblemThree();
             //ProblemFour();
-            ProblemFive();
-            //ProblemSix();
+            //ProblemFive();
+            ProblemSix();
             //ProblemSeven();
             //ProblemEight();
             //ProblemNine();
@@ -110,6 +110,16 @@ namespace DatabaseFirstLINQ
         {
             // Write a LINQ query that gets all of the users who registered AFTER 2016 and BEFORE 2018
             // Then print each user's email and registration date to the console.
+            var users = _context.Users;
+            var beforeDate = new DateTime(2018, 1, 1, 0, 0, 0);
+            var afterDate = new DateTime(2016, 1, 1, 0, 0, 0);
+            var usersBetween = users.Where(u => u.RegistrationDate > afterDate && u.RegistrationDate < beforeDate);
+
+            foreach (var user in usersBetween)
+            {
+                Console.WriteLine(user.Email + " " + user.RegistrationDate);
+            }
+
 
         }
 
